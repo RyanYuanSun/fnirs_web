22315aad09df9ba016544d7f82b313e9dbbd8da7
Ryan Alloriadonis <92444398+RyanYuanSun@users.noreply.github.com>
2023-10-09 10:48:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include_once "C:\Users\Administrator\Desktop\sunrays\web\php\session\session_ini.php";
+include_once "C:\Users\Administrator\Desktop\sunrays\web\php/traffic_log.php";
+header("Location:/business/fnirs2023/account");
+exit();
